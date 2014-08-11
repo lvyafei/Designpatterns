df2f01b002fa2ba9ff32c4722c82cec26476b6b4
lvyafei <lvyafeixx@gmail.com>
2014-08-11 14:32:58 +0800

    代理模式
    新增代理模式

---
+++ src/structureType/proxyPattern/IMath.java
@@ -0,0 +1,12 @@
+package structureType.proxyPattern;
+
+public interface IMath {
+	
+	double Add(double x, double y);
+
+	double Sub(double x, double y);
+
+	double Mul(double x, double y);
+
+	double Dev(double x, double y);
+}
--- src/test/testStructure.java
+++ src/test/testStructure.java
@@ -14,6 +14,7 @@ import structureType.bridgePattern.ConcreteImplementor1;
 import structureType.bridgePattern.Implementor;
 import structureType.bridgePattern.RefinedAbstraction;
 import structureType.decorativePattern.LowerCaseInputStream;
+import structureType.proxyPattern.MathProxy;
 
 public class testStructure {
 
@@ -62,5 +63,20 @@ public class testStructure {
 	public void Testadapter(){
 		Target target=new Adapter();
 		target.Request();
+	}
+	@Test
+	/**
+	 *代理模式
+	 */
+	public void Testproxy(){
+		MathProxy proxy = new MathProxy();
+        double addresult = proxy.Add(2,3);
+        System.out.println("2+3="+String.valueOf(addresult));
+        double subresult = proxy.Sub(2,3);
+        System.out.println("2-3="+String.valueOf(subresult));
+        double mulresult = proxy.Mul(2,3);
+        System.out.println("2*3="+String.valueOf(mulresult));
+        double devresult = proxy.Dev(2,3);
+        System.out.println("2/3="+String.valueOf(devresult));
 	}
 }
